f28d2b7ae5d5231a67cfc795e066f2b8cf4e7b31
Laurent Bernabe <laurent.bernabe@gmail.com>
2016-09-04 13:54:28 +0200

    The board component draws cells and coordinates.

--- src/main/java/com/loloof64/gluon/simple_chess_board/BoardComponent.java
+++ src/main/java/com/loloof64/gluon/simple_chess_board/BoardComponent.java
@@ -20,11 +20,19 @@ package com.loloof64.gluon.simple_chess_board;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 /**
  * The board component class.
  */
 class BoardComponent extends Canvas {
+
+    private static final int [] RANKS = new int[]{0,1,2,3,4,5,6,7};
+    private static final int [] FILES = new int[]{0,1,2,3,4,5,6,7};
+
+    private static final char [] RANKS_COORDS = new char[]{'1', '2', '3', '4', '5', '6', '7', '8'};
+    private static final char [] FILES_COORDS = new char[]{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
 
     BoardComponent(double cellsSize){
         super(9*cellsSize, 9*cellsSize);
@@ -35,12 +43,56 @@ class BoardComponent extends Canvas {
 
     private void draw(){
         drawBackground();
+        drawCells();
+        drawCoords();
     }
 
     private void drawBackground(){
         GraphicsContext gc = getGraphicsContext2D();
         gc.setFill(Color.BLUEVIOLET);
         gc.fillRect(0.0, 0.0, 9*cellsSize, 9*cellsSize);
+    }
+
+    private void drawCells(){
+        GraphicsContext gc = getGraphicsContext2D();
+        for (int rank: RANKS){
+            for (int file: FILES){
+                final boolean isWhiteCell = (file+rank)%2 != 0;
+                final double cellX = 0.5*cellsSize + file*cellsSize;
+                final double cellY = 0.5*cellsSize + (7-rank)*cellsSize;
+                final Color cellColor = isWhiteCell ? Color.LIGHTGOLDENRODYELLOW : Color.SADDLEBROWN;
+
+                gc.setFill(cellColor);
+                gc.fillRect(cellX, cellY, cellsSize, cellsSize);
+            }
+        }
+    }
+
+    private void drawCoords(){
+        GraphicsContext gc = getGraphicsContext2D();
+        final int fontSize = (int) (cellsSize * 0.7);
+        final Font font = Font.font(null, FontWeight.BOLD, fontSize);
+
+        gc.setFont(font);
+        gc.setFill(Color.BLUE);
+        for (int rank: RANKS){
+            final char currentCoordChar = RANKS_COORDS[rank];
+            final double x1 = 0.045*cellsSize;
+            final double x2 = 8.545*cellsSize;
+            final double y = 1.2*cellsSize + (7-rank)*cellsSize;
+
+            gc.fillText(String.format("%c", currentCoordChar), x1, y);
+            gc.fillText(String.format("%c", currentCoordChar), x2, y);
+        }
+        for (int file: FILES){
+            final char currentCoordChar = FILES_COORDS[file];
+            final double x = 0.745*cellsSize + file*cellsSize;
+            final double y1 = 0.5*cellsSize;
+            final double y2 = 9.0*cellsSize;
+
+            gc.fillText(String.format("%c", currentCoordChar), x, y1);
+            gc.fillText(String.format("%c", currentCoordChar), x, y2);
+        }
     }
 
     private double cellsSize;
